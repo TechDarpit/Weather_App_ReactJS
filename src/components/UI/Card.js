0523457e57bc1fa44252d7fd87f085a4d30d4bc7
Darpit Makwana <79750384+TechDarpit@users.noreply.github.com>
2022-02-21 00:17:09 +0530

    Delete src directory

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import styles from './Card.module.css';
-
-const Card = (props) => {
-  const cardClasses = `${props.className} ${styles.card}`;
-  return <div className={cardClasses}>{props.children}</div>;
-};
-
-export default Card;
